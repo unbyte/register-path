843cc23cb9850f19af28391eeb3100e2ac291021
unbyte <i@shangyes.net>
2026-09-14 22:54:51 +0800

    bootstrap

---
+++ .changeset/message.js
@@ -0,0 +1,11 @@
+export default {
+  async getVersionMessage(releasePlan, _options) {
+    const pkg = releasePlan.releases.find((release) => release.name === 'add-path')
+
+    if (!pkg) {
+      throw new Error(`main package not found in release plan`)
+    }
+
+    return `chore(release): ${pkg.newVersion}`
+  },
+}
